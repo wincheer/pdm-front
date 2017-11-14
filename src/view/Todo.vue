93bb5983c21bea95d51d65738fc9bb15be5b775d
wincheer <wincheer@126.com>
2017-11-14 17:53:10 +0800

    资源文件

--- src/view/Todo.vue
+++ src/view/Todo.vue
@@ -2,10 +2,20 @@
     <div>
         <el-row type="flex"  justify="center" align="middle">
             <el-col :span="16">
-                <img alt="正在建设中" src="../assets/doing.jpg" width="100%" height="100%">
+                <img alt="正在建设中" :src="doingIcon" width="100%" height="100%">
             </el-col>
         </el-row>
     </div>
 </template>
+<script>
+import doing from "../assets/doing.jpg"
+export default {
+  data(){
+      return{
+          doingIcon:doing
+      }
+  }
+}
+</script>
 
 
